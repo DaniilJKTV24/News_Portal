e1663e29b5ac09ad5fcd96b9bf77da8efda9e6e6
Daniil Peretjaka <daniil.peretjaka@ivkhk.ee>
2025-11-27 10:03:11 +0200

    Layout added

---
+++ admin/adminView/templates/layout.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="public/css/bootstrap.css">
+    <link rel="stylesheet" href="public/css/mystyle.css">
+    <link rel="stylesheet" href="public/css/font-awesome.min.css">
+    <script src="public/js/jquery.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="public/js/ajaxupload.3.5.js"></script>
+  </head>
+  <body>
+    <div class="container">
+
+      <div class="header clearfix">
+        <nav class="navbar navbar-default">
+          <div class="container-fluid"></div>
+        </nav>
+      </div>
+      
+      <div id="content" style="padding-top: 20px;"></div>
+
+      <footer class="footer">
+        <p>&copy; 2025 Design Admin Dashboard<i class="fa fa-child"></i></p>
+      </footer>
+      
+    </div>
+  </body>
+</html>
