b3b18d58b70383b402541ac23999e4f5b441847c
Daniil Peretjaka <daniil.peretjaka@ivkhk.ee>
2025-11-27 10:33:22 +0200

    Initialized admin module + layout update

---
+++ admin/adminView/adminStart.php
@@ -0,0 +1,17 @@
+<?php
+ob_start();
+?>
+
+<article>
+  <div id="main" class="container">
+    <h3>Admin panel</h3>
+    <div class="row">
+      <p>Admin panel</p>
+    </div>
+  </div>
+</article>
+
+<?php
+$content = ob_get_clean();
+include "adminView/templates/layout.php";
+?>
--- admin/adminView/templates/layout.php
+++ admin/adminView/templates/layout.php
@@ -11,18 +11,45 @@
   <body>
     <div class="container">
 
+      <?php
+      if (isset($_SESSION["userId"]) && isset($_SESSION["sessionId"])) {
+      ?>
+
       <div class="header clearfix">
         <nav class="navbar navbar-default">
-          <div class="container-fluid"></div>
+          <div class="container-fluid">
+
+            <?php
+            echo '<ul class="nav nav-pills pull-right">
+              <li role="button">' .$_SESSION["name"]. '
+                <a href="logout" style="display:inline;">Exit <i class="fa fa-sign-out"></i></a>
+              </li>
+            </ul>';
+
+            if (isset($_SESSION["status"]) && $_SESSION["status"] == "admin") {
+              echo '<h4><a href="../" target=blank>WEB SITE</a>';
+              echo ' &#187 <a href="adminCategory">Categories</a>';
+              echo ' &#187 <a href="adminNews">News List</a>';
+              echo '</h4>';
+            } else {
+              echo '<h4>You do not have the required permissions!</h4>';
+            }
+            ?>
+
+          </div>
         </nav>
       </div>
       
+      <?php
+      }
+      ?>
+
       <div id="content" style="padding-top: 20px;"></div>
 
       <footer class="footer">
         <p>&copy; 2025 Design Admin Dashboard<i class="fa fa-child"></i></p>
       </footer>
-      
+
     </div>
   </body>
 </html>
