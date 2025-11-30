6b3279202e2ac0afabdb1b4583b9f0ddaa96213e
unknown <daniil.peretjaka@ivkhk.ee>
2025-11-30 12:23:27 +0200

    Add registration confirmation page

---
+++ view/registerConfirmation.php
@@ -0,0 +1,32 @@
+<?php
+ob_start();
+?>
+<?php
+if (isset($result)) {
+  if ($result[0] == true) {
+    ?>
+    <div class="container">
+      <div class="alert alert-info">
+        <strong>User added.</strong>
+        <a href="admin/"> Dashboard</a>
+      </div>
+    </div>
+    <?php
+  }
+  else if ($result[0] == false) {
+    ?>
+    <div class="container">
+      <div class="alert alert-warning">
+        <strong>Error!</strong>
+        <?php echo $result[1];?>
+        <a href="registerForm"> Registration Form</a>
+      </div>
+    </div>
+    <?php
+  }
+}
+?>
+<?php
+$content = ob_get_clean();
+include 'view/layout.php';
+?>
