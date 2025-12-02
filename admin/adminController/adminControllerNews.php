6c9b3bd177d9b3e52bc77d338ed1e3b4a3bd4d7b
Daniil Peretjaka <daniil.peretjaka@ivkhk.ee>
2025-12-02 14:11:32 +0200

    feat(admin-news): enhance controller for adding news articles

--- admin/adminController/adminControllerNews.php
+++ admin/adminController/adminControllerNews.php
@@ -5,5 +5,15 @@ class adminControllerNews {
     $arr = adminModelNews::getNewsList();
     include_once 'adminView/newsList.php';
   }
+
+  public static function addNewsForm() {
+    $arr = adminModelCategory::getCategorylist();
+    include_once('adminView/addNewsForm.php');
+  }
+
+  public static function addNewsResult() {
+    $test = adminModelNews::getAddNews();
+    include_once('adminView/addNewsForm.php');
+  }
 }
 ?>
